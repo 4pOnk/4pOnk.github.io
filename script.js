bb3b725b5f4b4b322ebdd50c26bbb17205876a49
4pOnk <58631153+4pOnk@users.noreply.github.com>
2021-08-19 22:17:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let emojies = ["✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"];
+
+document.querySelector(".generate").addEventListener("click", () => {
+  let text = document.querySelector(".text_inp").value.split(" ");
+  let smiles = document.querySelector(".smiles_inp").value.split(" ");
+
+  let pasta = [];
+  let repeats = +document.querySelector(".repeats_inp").value;
+  for (let i = 0; i < repeats; i++)
+    text.forEach((val, id) => {
+      pasta.push(val.split("_").join(" "));
+      if(!document.querySelector('.checkbox').checked){
+        pasta.push(smiles[(i * text.length + id) % smiles.length]);
+      }else{
+        pasta.push(emojies[Math.floor(Math.random() * (emojies.length))]);
+      }
+    });
+
+  document.querySelector(".pasta").innerHTML = pasta.join(" ");
+
+  document.querySelector('.flex').style.display = 'flex';
+});
+
+document.querySelector("img").addEventListener("click", () => {
+  var copyText = document.querySelector(".pasta");
+  let range = new Range();
+
+  range.setStart(copyText, 0);
+  range.setEnd(copyText, 1);
+  document.getSelection().addRange(range);
+  document.execCommand("copy");
+  document.getSelection().removeAllRanges();
+});
